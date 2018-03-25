15ca9915a452c7e98109f2682af0372674729e39
ngai tong cheng <ngai tong cheng>
2018-03-25 14:55:11 +0800

    add limitation

--- uploadImage/views.py
+++ uploadImage/views.py
@@ -17,21 +17,29 @@ def upload_file(request):
     if request.method == 'POST':
             imgAuthor=Member.objects.get(username=request.POST.get("author",'none'))
             numImage=len(Image.objects.filter(author=imgAuthor))
+            newImg = Image()
+            newImg.title = request.POST.get("title",'')
+            if request.POST["title"]=='':
+                return HttpResponse("Upload failed: Please specify a title<br><a href='./'>back to upload page</a>")
+            newImg.description = request.POST.get("description",'')
+            if request.POST["description"]=='':
+                return HttpResponse("Upload failed: Please specify the description<br><a href='./'>back to upload page</a>")
+            newImg.category = request.POST.get("category",'')
+            if request.POST["tag"]=='':
+                return HttpResponse("Upload failed: Please specify a tag<br><a href='./'>back to upload page</a>")
+            if request.POST.get("image",'none')=='':
+                return HttpResponse("Upload failed: no image is selected<br><a href='./'>back to upload page</a>")
+            newImg.imageFile = request.FILES['image']
+            newImg.time = datetime.datetime.now()
+            newImg.numberOfView = 0
+            newImg.likes = 0
+            newImg.author=imgAuthor
             if numImage>=TOTAL_NUMBER_OF_AVAILABLE_IMAGE:
                 return HttpResponse("Upload failed: Number of images uploaded exceeds the limit.<br> <a href='./'>back to upload page</a>")
             if imgAuthor.uploadFrequency>=MAXIMUM_UPLOAD_FREQUENCY:
                 return HttpResponse("Upload failed: Number of images uploaded exceeds the daily limit.<br> <a href='./'>back to upload page</a>")
             imgAuthor.uploadFrequency+=1
             imgAuthor.save()
-            newImg = Image()
-            newImg.title = request.POST.get("title",'error')
-            newImg.description = request.POST.get("description",'')
-            newImg.category = request.POST.get("category",'')
-            newImg.imageFile = request.FILES['image']
-            newImg.time = datetime.datetime.now()
-            newImg.numberOfView = 0
-            newImg.likes = 0
-            newImg.author=imgAuthor
             newImg.save()
             tag=Tag.objects.filter(name=request.POST.get("tag",'none'))
             if not tag:
